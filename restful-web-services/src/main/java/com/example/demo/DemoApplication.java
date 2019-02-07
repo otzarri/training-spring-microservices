82ff405218f7afbb546559b4a5b6e63cd2d7822d
Joseba Martos <1229008-josebamartos@users.noreply.gitlab.com>
2019-02-07 00:05:44 -0500

    Replaced restful-web-services project with the correct one downloaded from Spring Initializr

--- restful-web-services/src/main/java/com/example/demo/DemoApplication.java
+++ restful-web-services/src/main/java/com/example/demo/DemoApplication.java
@@ -1,13 +1,13 @@
-package com.in28minutes.rest.webservices.restfulwebservices;
+package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class RestfulWebServicesApplication {
+public class DemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(RestfulWebServicesApplication.class, args);
+		SpringApplication.run(DemoApplication.class, args);
 	}
 
 }
--- restful-web-services/src/test/java/com/example/demo/DemoApplicationTests.java
+++ restful-web-services/src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,4 +1,4 @@
-package com.in28minutes.rest.webservices.restfulwebservices;
+package com.example.demo;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class RestfulWebServicesApplicationTests {
+public class DemoApplicationTests {
 
 	@Test
 	public void contextLoads() {
